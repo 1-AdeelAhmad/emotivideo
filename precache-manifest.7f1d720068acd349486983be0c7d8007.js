c3660590f016bf6eb701e189fe995c612914e66b
Adeel <adeel@wfgames.co.uk>
2020-06-06 16:19:09 +0100

    Updates

---
+++ precache-manifest.7f1d720068acd349486983be0c7d8007.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ffa22743c688a6962f5a2549c7acdb",
+    "url": "/emotivideo/index.html"
+  },
+  {
+    "revision": "7bec48055a66c350ae07",
+    "url": "/emotivideo/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "545127ea878a6fe238da",
+    "url": "/emotivideo/static/js/2.41fa1a21.chunk.js"
+  },
+  {
+    "revision": "1d223435c35525049989de575de07279",
+    "url": "/emotivideo/static/js/2.41fa1a21.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bec48055a66c350ae07",
+    "url": "/emotivideo/static/js/main.714587e4.chunk.js"
+  },
+  {
+    "revision": "7e7fc8b6670c65ffd654",
+    "url": "/emotivideo/static/js/runtime-main.fe0caa90.js"
+  }
+]);
